3a887f6543a42108c9142cadd734b7237731ff7e
Luis Giuliani <154923735+jrgiuliani@users.noreply.github.com>
2024-03-14 02:59:02 +0000

    entrega

--- src/app.js
+++ src/app.js
@@ -7,5 +7,22 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let quien = who[Math.floor(Math.random() * who.length)];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let accion = action[Math.floor(Math.random() * action.length)];
+  let what = ["my homework", "the keys", "the car"];
+  let que = what[Math.floor(Math.random() * what.length)];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let cuando = when[Math.floor(Math.random() * when.length)];
+
+  let excusePhrase = document.getElementById("excuse");
+  let newPhrase = quien + " " + accion + " " + que + " " + cuando;
+  excusePhrase.innerText = newPhrase;
 };
