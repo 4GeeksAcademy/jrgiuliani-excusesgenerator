1bb087a1bbd68b6048f0b5ae8941c0879a9022a3
Luis Giuliani <154923735+jrgiuliani@users.noreply.github.com>
2024-03-16 19:53:32 +0000

    1. Agregue comentarios para describir el codigo. 1. Agrupe los arreglos y las variables para contenido aleatorio. 3. Inclui el `${}` para simplificar el string y no sumar espacios. 4. Agregue la clase min-vh-100 al contenedor padre. 5. Cree un contenedor hijo y le agregue padding para centrar verticalmente.

--- src/app.js
+++ src/app.js
@@ -6,13 +6,13 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  //write your code here. Este código crea un generador de excusas, toma aleatoreamente elementos de los arreglos y los muestra en un string.
+
+  //Arreglos:
+
   let who = ["The dog", "My grandma", "His turtle", "My bird"];
-  let quien = who[Math.floor(Math.random() * who.length)];
   let action = ["ate", "peed", "crushed", "broke"];
-  let accion = action[Math.floor(Math.random() * action.length)];
   let what = ["my homework", "the keys", "the car"];
-  let que = what[Math.floor(Math.random() * what.length)];
   let when = [
     "before the class",
     "right on time",
@@ -20,9 +20,16 @@ window.onload = function() {
     "during my lunch",
     "while I was praying"
   ];
+  // Variables seleccion de texto aleatorio para cada arreglo.
+
+  let quien = who[Math.floor(Math.random() * who.length)];
+  let accion = action[Math.floor(Math.random() * action.length)];
+  let que = what[Math.floor(Math.random() * what.length)];
   let cuando = when[Math.floor(Math.random() * when.length)];
 
+  // Enlace con el archivo HTML a traves del id del <p> y modificacion con texto aleatorio.
+
   let excusePhrase = document.getElementById("excuse");
-  let newPhrase = quien + " " + accion + " " + que + " " + cuando;
+  let newPhrase = `${quien} ${accion} ${que} ${cuando}.`;
   excusePhrase.innerText = newPhrase;
 };
